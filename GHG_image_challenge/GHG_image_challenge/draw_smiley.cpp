db0f22acc55b639b5fd92247f849303593d84b53
cfinniga <cfinnig@gmail.com>
2021-01-01 12:21:59 -0800

    added exception handling

--- GHG_image_challenge/GHG_image_challenge/draw_smiley.cpp
+++ GHG_image_challenge/GHG_image_challenge/draw_smiley.cpp
@@ -9,25 +9,27 @@ int main(int argc, char *argv[]) {
 	// Parse input arguments
 	if (argc == 2) {
 		input_file_name = argv[1];
-		//output_file_name = argv[2];
 	}
 	else {
 		std::cout << "Usage: draw_smiley input_file" << std::endl;
 		return 0;
 	}
 
-	// read image
-	std::ifstream input_file(input_file_name, std::ifstream::in);
+	try {
+		// read image
+		std::ifstream input_file(input_file_name, std::ifstream::in);
+		pgm_image input_image(input_file);
 
-	pgm_image input_image(input_file);
+		// draw on image
+		input_image.draw_smiley();
 
-	// draw on image
-	input_image.draw_smiley();
+		// write image
+		input_image.write_pgm_file(output_file_name);
 
-	input_image.print_image();
-
-	// write image
-	input_image.write_pgm_file(output_file_name);
+	}
+	catch (...) {
+		std::cout << "Error: input file is corrupt" << std::endl;
+	}
 
 	return 0;
 }
